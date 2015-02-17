9fdb12c5fa176115e6c5045fb5dc3b85ded4fee1
Masahiro Nishiyama <qfwrd509@gmail.com>
2015-02-17 18:44:06 +0900

    フラグメントの一番簡単な利用方法

---
+++ FragmentSample2/app/src/main/java/com/example/masahironishiyama/fragmentsample2/Fragment2.java
@@ -0,0 +1,27 @@
+package com.example.masahironishiyama.fragmentsample2;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Fragment2.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Fragment2#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment2 extends android.support.v4.app.Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_fragment2, container, false);
+        return rootView;
+    }
+}
